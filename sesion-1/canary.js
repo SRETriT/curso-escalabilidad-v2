4801cb0593fef17773069952ac72046e5f114e5b
Felipe Polo Ruiz <felipe.polo@gmail.com>
2020-11-08 02:06:29 +0100

    Adapting for easy lambda deployment with serverless framework (#1)
    * Adapting for easy lambda deployment with serverless
    
    * Error handler missing
    
    * Another error handler missing

--- sesion-1/canary.js
+++ sesion-1/canary.js
@@ -1,18 +1,24 @@
 const redis = require('redis');
-const client = redis.createClient({
-	host: 'localhost',
-	port: 6379,
-});
 
-client.on('error', function(error) {
-	console.error(error);
-});
-
-client.get('canary', (error, result) => {
-	console.log(`Read canary ${result}`)
-	client.set('canary', 'alive', 'EX', 3 * 60, error => {
-		console.log('Canary refreshed')
-		client.quit()
+exports.handler = async event => new Promise((resolve, reject) => {
+	const client = redis.createClient({
+		host: process.env.REDIS_HOST || 'localhost',
+		port: 6379,
 	});
-})
-
+	
+	client.on('error', function(error) {
+		console.error(error);
+		reject(error);
+	});
+	
+	client.get('canary', (error, result) => {
+		if (error) return reject(error);
+		console.log(`Read canary ${result}`)
+		client.set('canary', 'alive', 'EX', 3 * 60, error => {
+			if (error) return reject(error);
+			console.log('Canary refreshed');
+			client.quit();
+			resolve();
+		});
+	})
+});
